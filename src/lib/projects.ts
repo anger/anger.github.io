74649e41d74b0402b134dd6155035ffd4dfce043
anger <48845671+anger@users.noreply.github.com>
2024-05-13 19:36:59 -0400

    refactor: Update featured projects in projects.ts

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,27 +1,26 @@
 export const featuredProjects = [
-    {
-      name: "Voyager.js",
-      href: "https://github.com/anger/voyager-js",
-      desc: "Automate the testing for template injection vulnerabilities",
-      aside: "JavaScript",
-    },
-    {
-      name: "fontpocalypse",
-      href: "https://github.com/anger/fontpocalypse",
-      desc: "Automated Deceptive Font Generation and HTML Masking",
-      aside: "Python, HTML/CSS",
-    },
-    {
-      name: "AngelFetch",
-      href: "https://github.com/anger/angelfetch",
-      desc: " An angel themed fetch script",
-      aside: "BASH",
-    },
-    {
-      name: "CTF Scripts",
-      href: "https://github.com/anger/ctf-scripts",
-      desc: "Collection of scripts I made for CTF challenges",
-      aside: "Python, JavaScript, C",
-    },
-
-  ];
+  {
+    name: "Voyager.js",
+    href: "https://github.com/anger/voyager-js",
+    desc: "Automate the testing for template injection vulnerabilities",
+    aside: "JavaScript",
+  },
+  {
+    name: "fontpocalypse",
+    href: "https://github.com/anger/fontpocalypse",
+    desc: "Automated Deceptive Font Generation and HTML Masking",
+    aside: "Python, HTML/CSS",
+  },
+  {
+    name: "CTF Challenges",
+    href: "https://github.com/anger/CTF-Challenges",
+    desc: "A collection of CTF challenges I've made",
+    aside: "JavaScript, C, Python",
+  },
+  {
+    name: "CTF Scripts",
+    href: "https://github.com/anger/ctf-scripts",
+    desc: "Collection of scripts I made for CTF challenges",
+    aside: "Python, JavaScript, C",
+  },
+];
